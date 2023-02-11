e4fb50cc7ecd4ecc2d71563dc57717813203d306
Keatonms <43686619+Keatonms@users.noreply.github.com>
2023-02-10 19:15:18 -0800

    Create changeColor.cs
    Changes the prefab GameObject color to indicate to the player that it is time to destroy the object.

---
+++ changeColor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        rend.material.color = Color.green;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        rend.material.color = Color.black;
+    }
+}
+
